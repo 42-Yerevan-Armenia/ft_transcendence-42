13ebfe88553fd52691a5be4908353ef08d921a16
Aram Khurshudyan <arakhurs@c2r11s2.42yerevan.am>
2024-02-05 19:24:51 +0400

    update

--- ft_transendence/backend/backend.sh
+++ ft_transendence/backend/backend.sh
@@ -3,10 +3,18 @@ python -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install pillow
-pip install djangorestframework
-pip install django-cors-headers
 echo 'source /app/backend/venv/bin/activate' >> /root/.bashrc
 echo "alias migrate='python manage.py makemigrations && python manage.py migrate'" >> /root/.bashrc
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
 echo "Starting Django Server, Enjoy!"
 python manage.py runserver 0.0.0.0:8000
+
+# c2r11s2% source venv/bin/activate
+# (venv) c2r11s2% python3 -V
+# Python 2.7.16
+# (venv) c2r11s2% docker exec -it backend /bin/bash
+# (venv) root@8c6e8c0300ab:/app/backend# source venv/bin/activate
+# (venv) root@8c6e8c0300ab:/app/backend# python -V
+# Python 3.10.13
+# (venv) c2r11s2% deactivate
