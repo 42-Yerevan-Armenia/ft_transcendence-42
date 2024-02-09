a26bb855af612b7289a15713599b00231bb483fc
Aram Khurshudyan <arakhurs@c2r11s2.42yerevan.am>
2024-02-09 14:42:00 +0400

    for merge

--- ft_transendence/backend/backend.sh
+++ ft_transendence/backend/backend.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-echo 'source /app/backend/venv/bin/activate' >> /root/.bashrc
-echo "alias migrate='python manage.py makemigrations && python manage.py migrate'" >> /root/.bashrc
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 echo "Starting Django Server, Enjoy!"
