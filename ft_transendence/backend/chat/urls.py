45f3b5ae345e217d9c8e7b387f55e4a36c4b7319
Aram Khurshudyan <arakhurs@c2r11s2.42yerevan.am>
2024-03-09 15:22:28 +0400

    SettingsById, Core->User relationship

--- ft_transendence/backend/chat/urls.py
+++ ft_transendence/backend/chat/urls.py
@@ -9,8 +9,7 @@ urlpatterns = [
 	path("", chat_views.chatPage, name="chat-page"),
 
 	# login-section
-	path("auth/login/", LoginView.as_view
-		(template_name="loginPage.html"), name="login-user"),
+	path("auth/login/", LoginView.as_view(template_name="loginPage.html"), name="login-user"),
 	path("auth/logout/", LogoutView.as_view(), name="logout-user"),
 ]
 
--- ft_transendence/backend/core/models.py
+++ ft_transendence/backend/core/models.py
@@ -10,7 +10,14 @@ class Person(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     password = models.CharField(max_length=100)
     image = models.TextField(blank=True, null=True)
-    
+    background = models.TextField(blank=True, null=True)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    matches = models.IntegerField(default=0)
+    gamemode = models.CharField(max_length=100, default='classic')
+    live = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='person')
+
     def __str__(self):
         return self.nickname
 
@@ -20,7 +27,18 @@ class Person(models.Model):
             self.image = base64_string
             self.save()
 
+    def save_base64_background(self, background_path=None, background_format='jpg'):
+        if background_path:
+            base64_string = self.background_to_base64(background_path)
+            self.background = base64_string
+            self.save()
+
     def image_to_base64(self, image_path):
         with open(image_path, "rb") as image_file:
             base64_string = base64.b64encode(image_file.read()).decode("utf-8")
+        return base64_string
+
+    def background_to_base64(self, background_path):
+        with open(background_path, "rb") as background_file:
+            base64_string = base64.b64encode(background_file.read()).decode("utf-8")
         return base64_string
