fcf3329b4dbbe8255ab1927dc40261f8c32cfc6a
Aram Khurshudyan <arakhurs@c2r11s2.42yerevan.am>
2024-04-24 22:02:26 +0400

    update_04

--- ft_transendence/backend/game/urls.py
+++ ft_transendence/backend/game/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
 from game import views as game_views
-from game.views import PlayRandom, PlayTournament
+from game.views import (
+    PlayRandom,
+    PlayTournament,
+    SendInviteRequest,
+    AcceptInviteRequest,
+    RejectInviteRequest
+)
 
 urlpatterns = [
     path('game/', game_views.index, name='index'),
     path('joinlist/', game_views.joinlist, name='joinlist'),
     path('tournament/', PlayTournament.as_view()),
     path('api/v1/random/<int:pk>/', PlayRandom.as_view()),
+    path('api/v1/invite/<int:pk>/', SendInviteRequest.as_view()),
+    path('api/v1/acceptinvite/<int:pk>/', AcceptInviteRequest.as_view()),
+    path('api/v1/rejectinvite/<int:pk>/', RejectInviteRequest.as_view()),
 ]
